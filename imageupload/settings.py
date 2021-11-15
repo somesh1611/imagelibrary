9a4d98e5f335fa6fdd908a8809c0678f9c691176
somesh1611 <gawadesomesh402@gmail.com>
2021-11-15 15:02:52 +0530

    Updated requirements.txt

--- imageupload/settings.py
+++ imageupload/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +130,5 @@ MEDIA_ROOT = BASE_DIR/'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
