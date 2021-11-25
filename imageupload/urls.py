45bd5c4cdcdc5d0024a9c18f7134edb01108b631
somesh1611 <gawadesomesh402@gmail.com>
2021-11-26 00:55:37 +0530

    updated site

--- imageupload/urls.py
+++ imageupload/urls.py
@@ -21,4 +21,5 @@ from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index)
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
